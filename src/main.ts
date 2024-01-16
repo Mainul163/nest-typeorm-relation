ce906a0f7af743735ae134eb59555a5452eba484
unknown <mainulisraf163@gmail.com>
2024-01-16 12:46:17 +0600

    otp work done

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -6,14 +6,35 @@ export class TwilioService {
   private client: Twilio.Twilio;
 
   constructor() {
-    this.client = Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+    const TWILIO_SID = 'AC1204c111277035f7fdf14a3da7c01da0';
+    const TWILIO_AUTH_TOKEN = '2cbfaa2e33372cc21640bc1f4dd63065';
+
+    this.client = Twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
   }
 
-  async sendOTP(phoneNumber: string, otp: string): Promise<void> {
-    await this.client.messages.create({
-      body: `Your OTP is: ${otp}`,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: phoneNumber,
-    });
+  async sendOTP(
+    phoneNumber: string,
+    otp: string,
+    retries: number = 3,
+  ): Promise<void> {
+    console.log(phoneNumber, 'phone ..........');
+    while (retries > 0) {
+      try {
+        await this.client.messages.create({
+          body: `Your OTP is: ${otp}`,
+          from: '+14083380771',
+          to: phoneNumber,
+        });
+        return; // Successfully sent OTP, exit the loop
+      } catch (error) {
+        console.error('Twilio API request failed:', error);
+        retries--;
+        if (retries === 0) {
+          throw error; // If retries exhausted, throw the error
+        }
+        // Wait for a moment before retrying
+        await new Promise((resolve) => setTimeout(resolve, 18000));
+      }
+    }
   }
 }
